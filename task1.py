bd3efedd19a0be5434f4e122dfa291f9caed2145
Sdsswt <terkeurswillem@gmail.com>
2025-01-07 11:29:11 -0800

    done

--- task1.py
+++ task1.py
@@ -1,6 +1,18 @@
 def A():
+    print("hello")
 
 def B():
-
+     print("how are you")
 
 def C():
+      print("hi")
+
+
+Letter = input("enter a letter - A, B, or C: ")
+
+if Letter == 'A':
+     A()
+if Letter == 'B':
+     B()
+if Letter == 'C':
+     C()
--- task2.py
+++ task2.py
@@ -8,3 +8,21 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+def title():
+    print("game rules: chose a number between 1 and 10,\n if the number is the same random number that you picked it will tell you chose the right number,\n if you dont chose the same number\n it will tell you that you chose the wrong number")
+
+def game():
+   y = int(input("enter a number between 1 and 10: "))
+   x = (random.randint(0,11))
+   while x!=y:
+    if x != y:
+       print("you chose the wrong number")
+       y = int(input("enter a number between 1 and 10: "))
+    if x == y:
+        print("you chose the right number")
+        break
+
+
+title()
+game()
